46df5bbc5df530c52aa2f5da560f27c96794001e
root <root@localhost.localdomain>
2018-12-07 22:29:51 +0300

    Create Paint Listener

--- src/main/java/SkelInterfaceB.java
+++ src/main/java/SkelInterfaceB.java
@@ -2,7 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class SkelInterfaceB {
+public class SkelInterfaceB implements ActionListener {
+
+    JFrame frame;
+
     public static void main (String[] args) {
 
         SkelInterfaceB gui = new SkelInterfaceB();
@@ -32,6 +35,20 @@ public class SkelInterfaceB {
 
 class MyDrawPanel extends JPanel {
     public void paintComponent (Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
         
+        int red = (int) (Math.random() * 255);
+        int green = (int) (Math.random() * 255);
+        int blue = (int) (Math.random() * 255);
+        Color startColour = new Color(red, green, blue);
+
+        red = (int) (Math.random() * 255);
+        green = (int) (Math.random() * 255);
+        blue = (int) (Math.random() * 255);
+        Color endColour = new Color(red, green, blue);
+
+        GradientPaint gradient = new GradientPaint(70, 70, startColour, 150, 150, endColour);
+        g2d.setPaint(gradient);
+        g2d.fillOval(70, 70, 100, 100);
     }
 }
