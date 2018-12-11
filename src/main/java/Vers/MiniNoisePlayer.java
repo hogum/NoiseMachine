e99d51e2a2822b6dc69a8a0a56d4609dfa9e75a5
root <root@localhost.localdomain>
2018-12-11 12:03:02 +0300

    Register and listen for MIDI events

--- src/main/java/Vers/MiniNoisePlayer.java
+++ src/main/java/Vers/MiniNoisePlayer.java
@@ -1,22 +1,32 @@
 
-// Version One
+// Version Two
 
 import javax.sound.midi.*;
 
-class MiniNoisePlayer {
+public class MiniNoisePlayer implements ControllerEventListener {
     
     public static void main(String[] args) {
+            MiniNoisePlayer player = new MiniNoisePlayer();
+            player.go();
+    }
 
+    public void go() {  
         try {
+                
                 Sequencer sequencer = MidiSystem.getSequencer();
                 sequencer.open();
 
+                int[] eventsControlled = {127};
+                sequencer.addControllerEventListener(this, eventsControlled);
                 Sequence sequence = new Sequence(Sequence.PPQ, 4);
                 Track track = sequence.createTrack();
 
                 for (int i = 5; i < 60; i+=4) {
                     track.add(createEvent(144, 1, i, 100, i));
-                    track.add(createEvent(144, 1, i, 100, 1));
+
+                    // Controller Event 176
+                    track.add(createEvent(176, 1, 127, 0, i));
+                    track.add(createEvent(144, 1, i, 100, i + 2));
 
             }
 
@@ -27,24 +37,26 @@ class MiniNoisePlayer {
         } catch (Exception ex) {
             System.out.println("Oops! Falied to create PLayer\n" + ex);
         }
-        
     }
-
-
-
-public static MidiEvent createEvent(int pow, int chan, int note, int strength, int time) {
-    MidiEvent event =null;
-
-    try {
-        ShortMessage msgOne = new ShortMessage();
-
-        msgOne.setMessage(pow, chan, note, strength);
-        event = new MidiEvent(msgOne, time);
     
-    } catch (Exception ex) {
-        System.out.println("Something bad happened" + ex);
-    }
+    public void controlChange(ShortMessage event) {
+                System.out.println("la");
+            }
 
-    return event;
-}
+
+    public static MidiEvent createEvent(int pow, int chan, int note, int strength, int time) {
+        MidiEvent event =null;
+
+        try {
+            ShortMessage msgOne = new ShortMessage();
+
+            msgOne.setMessage(pow, chan, note, strength);
+            event = new MidiEvent(msgOne, time);
+        
+        } catch (Exception ex) {
+            System.out.println("Something bad happened" + ex);
+        }
+
+        return event;
+    }
 }
