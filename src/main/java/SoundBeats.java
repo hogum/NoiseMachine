0e2ef4ccae55da2d60b969187b604a5521ea0d66
ks <mugoh.ks@gmail.com>
2018-12-17 21:26:38 +0300

    Add Midis and events

--- src/main/java/SoundBeats.java
+++ src/main/java/SoundBeats.java
@@ -2,19 +2,30 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 
 import javax.sound.midi.*;
 
 import java.util.ArrayList;
 
+import java.awt.BorderLayout;
+import java.awt.Label;
+import java.awt.GridLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
 public class SoundBeats {
    
     JFrame frame;
     JPanel beatPanel;
-    Sequencer sequncer;
-    Sequence sequnce;
+    Sequencer sequencer;
+    Sequence sequence;
     Track track;
-    ArrayList<JCheckBox> checkBoxes;
+    ArrayList<JCheckBox> checkBoxesList;
 
     String [] soundNames = {
         "Bass Drum", "Closed Hi-Hat",
@@ -29,6 +40,200 @@ public class SoundBeats {
 
     public static void main(String[] args) {
         
+        SoundBeats sb = new SoundBeats();
+        sb.buildGui();
+    }
+
+
+    public void buildGui() {
+        frame = new JFrame("Sound Box");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        BorderLayout bLayout = new BorderLayout();
+
+        JPanel background = new JPanel(bLayout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        checkBoxesList = new ArrayList<JCheckBox>();
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+
+        JButton startButton =  new JButton("Start");
+        startButton.addActionListener(new StartButtonListener());
+        buttonBox.add(startButton);
+
+        JButton stopButton =  new JButton("Stop");
+        stopButton.addActionListener(new StopButtonListener());
+        buttonBox.add(stopButton);
+
+        JButton tempoUpButton =  new JButton("Tempo Up");
+        tempoUpButton.addActionListener(new TempoUpButtonListener());
+        buttonBox.add(tempoUpButton);
+
+        JButton tempoDownButton =  new JButton("Tempo Down");
+        tempoDownButton.addActionListener(new TempoDownButtonListener());
+        buttonBox.add(tempoDownButton);
+
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+
+        for (int i=0; i < 16 ; i++)
+            nameBox.add(new Label(soundNames[i]));
+        
+        background.add(BorderLayout.WEST, buttonBox);
+        background.add(BorderLayout.EAST, nameBox);
+
+        frame.getContentPane().add(background);
+
+        GridLayout gridLayout = new GridLayout(16, 16);
+        //gridLayout.setVgap(1);
+        gridLayout.setHgap(2);
+        
+        beatPanel = new JPanel(gridLayout);
+        background.add(BorderLayout.CENTER, beatPanel);
+
+        for (int i=0; i<= 255; i++) {
+            JCheckBox checkB = new JCheckBox();
+            checkB.setSelected(false);
+            checkBoxesList.add(checkB);
+            beatPanel.add(checkB);
+        }
+
+        setUpMidi();
+
+        frame.setBounds(350, 90, 300, 300);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public void setUpMidi() {
+        try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ, 4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+        
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void buildTracks(int[] trackList) {
+
+        for (int i=0; i<=15; i++) {
+            int key = trackList[i];
+
+            if (key != 0) {
+                track.add(createEvent(144, 9, key, 100, i));
+                track.add(createEvent(128, 9, key, 100, i+1));
+            }
+        }
+    }
+
+    public MidiEvent createEvent(int command, int channel, int key, int stroke, int time) {
+
+        MidiEvent event = null;
+
+        try {
+            ShortMessage midiMsg = new ShortMessage();
+            midiMsg.setMessage(
+                command,
+                channel,
+                key,
+                stroke);
+            event = new MidiEvent(midiMsg, time);
+        
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return event;
+    }
+
+
+    public void startTracks() {
+    
+    /*
+    Creates a 16-element array to hold values for each track.
+    Value of an intrument that's to play is set as the 'note',
+    if not, the value is zero.
+    */
+
+        int [] trackList = null;
+
+        // Clear old track
+
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+
+        for (int i=0; i<=15; i++) {
+            
+            trackList = new int[16];
+
+            int note = soundNotes[i];
+
+            for (int j=0; j<=15; j++) {
+
+                JCheckBox checkB = (JCheckBox) checkBoxesList.get(j + (16*i));
+
+                if (checkB.isSelected()) {
+                    trackList[j] = note;
+                
+                } else {
+
+                    trackList[j] = 0;
+                }
+            }
+
+            buildTracks(trackList);
+            track.add(createEvent(176, 1, 127, 0, 16));
+        }
+
+        track.add(createEvent(192, 9, 1, 0, 15));
+
+        try {
+
+                sequencer.setSequence(sequence);
+                sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);
+                sequencer.start();
+                sequencer.setTempoInBPM(120);
+        
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+        }
+    }
+
+
+    class StopButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            
+            sequencer.stop();
+        }
+    }
+
+    class StartButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent event) {
+            startTracks();
+        }
+
+    }
+
+     class TempoUpButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+            
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) (tempoFactor * 1.03));
+        }
+    }
+
+     class TempoDownButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
+           
+            float tempoFactor = sequencer.getTempoFactor();
+            sequencer.setTempoFactor((float) (tempoFactor * .97));
+        }
     }
 
 }
