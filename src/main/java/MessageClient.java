d2d36e7f964b8a5c68190e77efc2d0ec684fe671
root <mugoh.ks@gmail.com>
2018-12-20 08:23:22 +0300

    Create Gui

--- src/main/java/MessageClient.java
+++ src/main/java/MessageClient.java
@@ -5,6 +5,11 @@ import javax.swing.JPanel;
 import javax.swing.JFrame;
 import javax.swing.JButton;
 
+import java.awt.BorderLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 import java.io.PrintWriter;
 
 import java.net.Socket;
@@ -23,6 +28,33 @@ public class MessageClient {
     Socket socket;
 
     public void start() {
+
+        JFrame frame = new JFrame("Might be a Messaging Client");
+        JPanel panel = new JPanel();
+        outgoingMessage =  new JTextField(20);
+        JButton sendButton = new JButton("Send");
+
+        sendButton.addActionListener(new SendButtonListener());
+
+        panel.add(outgoingMessage);
+        panel.add(sendButton);
+
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.setSize(400, 500);
+        frame.setVisible(true);
+    }
+
+    public class SendButtonListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent ev) {
+
+
+        }
+    }
+
+    public static void main(String[] args) {
         
+        MessageClient mc = new MessageClient();
+        mc.start();
     }
 }
