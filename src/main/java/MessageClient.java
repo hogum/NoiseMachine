4e3f9bc6927cb5017f131b33de92297a52e29843
root <mugoh.ks@gmail.com>
2018-12-20 08:01:03 +0300

    Create client example

---
+++ src/main/java/MessageClient.java
@@ -0,0 +1,28 @@
+// Version 1 // Send only
+
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+import java.io.PrintWriter;
+
+import java.net.Socket;
+
+/*
+
+    Reads text from a text field
+    and sends it to a socket.
+
+*/
+
+public class MessageClient {
+
+    JTextField outgoingMessage;
+    PrintWriter printer;
+    Socket socket;
+
+    public void start() {
+        
+    }
+}
