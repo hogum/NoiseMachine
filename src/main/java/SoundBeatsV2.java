5649b3e6ae6bcfc963f9a9f314c65fe6c7930e57
root <mugoh.ks@gmail.com>
2018-12-22 10:36:07 +0300

    Start on Complete version of SoundBeats

---
+++ src/main/java/SoundBeatsV2.java
@@ -0,0 +1,32 @@
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JFileChooser;
+
+import javax.sound.midi.*;
+
+import java.util.ArrayList;
+
+import java.awt.BorderLayout;
+import java.awt.Label;
+import java.awt.GridLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.IOException;
+
+
+public class SoundBeatsV2 {
+
+    
+}
+
